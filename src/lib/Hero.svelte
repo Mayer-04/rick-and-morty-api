2642c493706df05c0300783d20db3e3378635a8f
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-25 21:00:02 -0500

    Añadiendo componente Hero

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import RickIcon from "./icons/RickIcon.svelte";
+</script>
+
+<section class="hero">
+  <h1>The Rick and Morty API</h1>
+  <figure>
+    <RickIcon></RickIcon>
+  </figure>
+</section>
+
+<style>
+  .hero {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    height: calc(50vh - 60px);
+  }
+
+  figure {
+    position: absolute;
+    width: 18.75rem;
+  }
+
+  h1 {
+    font-size: 6.25rem;
+    font-weight: 900;
+    z-index: 10;
+    text-align: center;
+  }
+
+  h1::selection {
+    background-color: var(--main);
+    color: var(--orange);
+  }
+</style>
